49362822d53d76406d9d1cfef604600849129509
Neo <24300240169@m.fudan.edu.cn>
2026-05-24 14:36:21 +0800

    shop status set and get

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,29 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建RedisTemplate对象...");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        // 设置Redis连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 设置redisKey序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // 设置redisValue序列化器
+        //redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
